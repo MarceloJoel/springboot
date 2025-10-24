5ac1abfe1c8b80d268c8af536b61ce076523c48f
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-10-24 01:55:29 -0500

    feat(EjemploRestController): add RequestMapping

--- src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploRestController.java
+++ src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploRestController.java
@@ -3,13 +3,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
-public class EjemploRestController {
-    
-    @GetMapping("/detalles_info2")
+@RequestMapping("/api")
 
+public class EjemploRestController {
+
+    //@RequestMapping(path = "/detalles_info2", method = RequestMethod.GET)
+    @GetMapping("/detalles_info2")
+    
     public Map<String, Object> detalles_info2() {
         Map<String, Object> respuesta = new HashMap<>(); 
         respuesta.put("Titulo", "Servidor en linea");
