d53abfa95048404d558a79e2fe5fd4d477fc6b9d
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-10-23 20:22:18 -0500

    Initial commit

---
+++ src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploController.java
@@ -0,0 +1,19 @@
+package com.proyectoinformatico.spring.springboot.controllers;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EjemploController {
+    @GetMapping("/detalles_info")
+
+    public String info(Map<String, Object> modelo) {
+        modelo.put("Titulo", "Servidor en linea");
+        modelo.put("Servidor", "Informaticonfig");
+        modelo.put("Ip", "192.168.1.1");
+        return "detalles_info";
+
+    }
+
+}
