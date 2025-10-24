2d5dbcc323cb1c801feaf6c6b4f9b5c786034aca
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-10-24 07:20:47 -0500

    feat: add ClaseDTO

--- src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploRestController.java
+++ src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.proyectoinformatico.spring.springboot.models.Empleados;
+import com.proyectoinformatico.spring.springboot.models.dto.ClaseDTO;
 
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,18 +19,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class EjemploRestController {
 
     //@RequestMapping(path = "/detalles_info2", method = RequestMethod.GET)
-    @GetMapping("/detalles_info2")
+    @GetMapping(path = "/detalles_info2")
     
-    public Map<String, Object> detalles_info2() {
-        Empleados empleado1 = new Empleados("Juan","Rodriguez", "Calle 123", "Gerente",
-                                        35, 123456789,001);
+    public ClaseDTO detalles_info2() {
+        // Empleados empleado1 = new Empleados("Juan","Rodriguez", "Calle 123", "Gerente",
+        //                                 35, 123456789,001);
         
-        Map<String, Object> respuesta = new HashMap<>(); 
-        respuesta.put("Empleado", "Datos empleado");
-        respuesta.put("Informacion", empleado1);
+        // Map<String, Object> respuesta = new HashMap<>(); 
+        // respuesta.put("Empleado", "Datos empleado");
+        // respuesta.put("Informacion", empleado1);
         
+        ClaseDTO usuario1 = new ClaseDTO();
+        usuario1.setTitulo("Administrador");
+        usuario1.setUsuario("Informaticconfig");
 
-        return respuesta;
+        return usuario1;
         
     }
 
