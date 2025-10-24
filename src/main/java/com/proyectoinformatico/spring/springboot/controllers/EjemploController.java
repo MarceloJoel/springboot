8f7c0535492da184a05d34ce01157d5c309d7655
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-10-24 02:17:41 -0500

    refactor: work with employed model "Empleado"

--- src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploController.java
+++ src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploController.java
@@ -2,16 +2,21 @@ package com.proyectoinformatico.spring.springboot.controllers;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.proyectoinformatico.spring.springboot.models.Empleados;
+
 
 @Controller
 public class EjemploController {
     @GetMapping("/detalles_info")
 
-    public String info(Map<String, Object> modelo) {
-        modelo.put("Titulo", "Servidor en linea Marcelo");
-        modelo.put("Servidor", "Informaticonfiguration :D");
-        modelo.put("Ip", "192.168.1.1");
+    public String info(Model model) {
+
+        Empleados empleado1 = new Empleados("Juan","Rodriguez", "Calle 123", "Gerente",
+        35, 123456789,001);
+        model.addAttribute("Empleado", empleado1);
         return "detalles_info";
 
     }
--- src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploRestController.java
+++ src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploRestController.java
@@ -5,6 +5,9 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.proyectoinformatico.spring.springboot.models.Empleados;
+
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,10 +21,14 @@ public class EjemploRestController {
     @GetMapping("/detalles_info2")
     
     public Map<String, Object> detalles_info2() {
+        Empleados empleado1 = new Empleados("Juan","Rodriguez", "Calle 123", "Gerente",
+                                        35, 123456789,001);
+        
         Map<String, Object> respuesta = new HashMap<>(); 
-        respuesta.put("Titulo", "Servidor en linea");
-        respuesta.put("Servidor", "Informaticonfig");
-        respuesta.put("Ip", "192.168.1.1");
+        respuesta.put("Empleado", "Datos empleado");
+        respuesta.put("Informacion", empleado1);
+        
+
         return respuesta;
         
     }
