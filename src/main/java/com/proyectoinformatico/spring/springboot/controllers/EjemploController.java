0dacbadb543a8fc44acdd8b810800496c68decab
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-10-25 17:06:55 -0500

    feat(EjemploController): add ModelAttribute

--- src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploController.java
+++ src/main/java/com/proyectoinformatico/spring/springboot/controllers/EjemploController.java
@@ -1,12 +1,15 @@
 package com.proyectoinformatico.spring.springboot.controllers;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.proyectoinformatico.spring.springboot.models.Empleados;
-
 
 @Controller
 public class EjemploController {
@@ -14,11 +17,21 @@ public class EjemploController {
 
     public String info(Model model) {
 
-        Empleados empleado1 = new Empleados("Juan","Rodriguez", "Calle 123", "Gerente",
-            35, 123456789,001);
+        Empleados empleado1 = new Empleados("Juan", "Rodriguez", null, "Gerente",
+                35, 123456789, 001);
         model.addAttribute("Empleado", empleado1);
         return "detalles_info";
+    }
 
+    @ModelAttribute("Empleados")
+    public List<Empleados> ListaEmpleados() {
+        return Arrays.asList(
+                new Empleados("Maria", "Perez", "Calle segunda no2", "Secretaria", 20, 123456798, 02),
+                new Empleados("María", "Perez", "Calle segunda no.2", "Secretaria", 20, 123654987, 02),
+                new Empleados("Juan", "Rodriguez", "Calle primera no.1", "Gerente", 35, 123456789, 01),
+                new Empleados("Luis", "Martínez", "Calle tercera no.3", "Contador", 28, 987654321, 03),
+                new Empleados("Ana", "González", "Avenida central no.4", "Diseñadora", 30, 456789123, 04),
+                new Empleados("Pedro", "Sánchez", "Boulevard no.5", "Analista", 25, 789123456, 05));
     }
 
 }
