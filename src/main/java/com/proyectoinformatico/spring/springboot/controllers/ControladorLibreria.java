8b611ac8651389f8c83d0ea415d0974a95510ddc
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-10-26 22:13:02 -0500

    fear: add Libros and RepoLibros

--- src/main/java/com/proyectoinformatico/spring/springboot/controllers/ControladorLibreria.java
+++ src/main/java/com/proyectoinformatico/spring/springboot/controllers/ControladorLibreria.java
@@ -1,11 +1,16 @@
 package com.proyectoinformatico.spring.springboot.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.proyectoinformatico.spring.springboot.models.Libros;
 import com.proyectoinformatico.spring.springboot.models.ServicioLibro;
+import com.proyectoinformatico.spring.springboot.repoLibros.RepoLibros;
 
 @RestController
 @RequestMapping("/api/libros")
@@ -13,14 +18,30 @@ import com.proyectoinformatico.spring.springboot.models.ServicioLibro;
 public class ControladorLibreria {
 
     private final ServicioLibro libroServicio; //final no puede cambiar de estado
+    private final RepoLibros libroRepositorio;
 
-    public ControladorLibreria(ServicioLibro libroServicio) {
+    public ControladorLibreria(ServicioLibro libroServicio, RepoLibros libroRepositorio) {
         this.libroServicio = libroServicio;
+        this.libroRepositorio = libroRepositorio;
     }
 
     @GetMapping("/{titulo}") //No hay setters
     public String DameLibro(@PathVariable String titulo) {
         return libroServicio.buscaLibro(titulo);
     }
+
+    @GetMapping("/todos")
+    public List<Libros>dimeTodos(){
+        return libroRepositorio.findAll();
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Libros>buscaId(@PathVariable long id){
+        return libroRepositorio.buscaId(id)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+    }
+    
+    
 
 }
---
+++ src/main/java/com/proyectoinformatico/spring/springboot/models/Libros.java
@@ -0,0 +1,52 @@
+package com.proyectoinformatico.spring.springboot.models;
+
+import java.time.LocalDate;
+
+public class Libros {
+    private long idLibro;
+    private String autor;
+    private String titulo;
+    private LocalDate fechaPublicacion;
+
+    public Libros(long idLibro, String autor, String titulo, LocalDate fechaPublicacion) {
+        this.idLibro = idLibro;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public long getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(long idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public LocalDate getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalDate fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    
+
+}
